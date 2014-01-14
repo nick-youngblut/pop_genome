d6d1bd6c673b583f48ecde31e03a26c751986bbd
Nick Youngblut <nyoungb2@illinois.edu>
2014-01-14 07:40:18 -0600

    quartetDecomp_byPop: just quartes w/ 2 taxa from each clade

--- blib/script/PopGen-db_loadSeqID.pl
+++ blib/script/PopGen-db_loadSeqID.pl
@@ -1,5 +1,79 @@
 #!/usr/bin/env perl
 
+=pod
+
+=head1 NAME
+
+PopGen-db_loadSeqID.pl -- loading a table of SeqID (or pdistance) values
+
+=head1 SYNOPSIS
+
+PopGen-db_loadSeqID.pl [flags] < pdist_res.txt
+
+=head2 Required flags
+
+=over
+
+=item -database  <char>
+
+PopGen-db database file.
+
+=item -runID  <char>
+
+ITEP cluster run ID. 
+
+=back
+
+=head2 Optional flags
+
+=over
+
+=item -regex  <char>
+
+Regex for pulling cluster ID out of colunn 1 values. [".+_|\.f[astn]*a\$"]
+
+=item -pdist  <bool>
+
+pdistance values instead of Sequence ID values? [FALSE]
+
+=item -verbose  <bool>
+
+Verbose output. [FALSE]
+
+=item -h	This help message
+
+=back
+
+=head2 For more information:
+
+perldoc PopGen-db_loadSeqID.pl
+
+=head1 DESCRIPTION
+
+Load table of sequence identities into PopGen-db.
+
+=head1 EXAMPLES
+
+=head2 Basic usage (pdistance values)
+
+PopGen-db_loadSeqID.pl -p -da PopGen-db.sqlite -r mazei_I_2.0_c_0.4_m_maxbit < pdist_res.txt
+
+=head1 AUTHOR
+
+Nick Youngblut <nyoungb2@illinois.edu>
+
+=head1 AVAILABILITY
+
+sharchaea.life.uiuc.edu:/home/git/ITEP_PopGen/
+
+=head1 COPYRIGHT
+
+Copyright 2010, 2011
+This software is licensed under the terms of the GPLv3
+
+=cut
+
+
 ### modules
 use strict;
 use warnings;
@@ -73,8 +147,11 @@ sub load_clust_info{
 			undef $l[2];
 			}
 		else{
-			$l[2] = (1 - $l[2]) * 100 if $pdist_b;			# converting to seqID
+			$l[2] = (100 - $l[2]) if $pdist_b;			# converting to seqID
 			}
+			
+		die "ERROR: $l[2] is not in range 0-100 (line $.)\n"
+			unless $l[2] >=0 && $l[2] <= 100;
 		
 		
 		$sth->execute($clusterID, $runID, $l[1], $l[2]);
@@ -97,79 +174,4 @@ sub check_file_IO{
 		unless -e $infile;
 	}
 
-
-__END__
-
-=pod
-
-=head1 NAME
-
-PopGen-db_loadSeqID.pl -- loading a table of SeqID (or pdistance) values
-
-=head1 SYNOPSIS
-
-PopGen-db_loadSeqID.pl [flags]
-
-=head2 Required flags
-
-=over
-
-=item -database  <char>
-
-PopGen-db database file.
-
-=item -runID  <char>
-
-ITEP cluster run ID. 
-
-=back
-
-=head2 Optional flags
-
-=over
-
-=item -regex  <char>
-
-Regex for pulling cluster ID out of colunn 1 values. [".+_|\.f[astn]*a\$"]
-
-=item -pdist  <bool>
-
-pdistance values instead of Sequence ID values? [FALSE]
-
-=item -verbose  <bool>
-
-Verbose output. [FALSE]
-
-=item -h	This help message
-
-=back
-
-=head2 For more information:
-
-perldoc PopGen-db_loadSeqID.pl
-
-=head1 DESCRIPTION
-
-Load table of sequence identities into PopGen-db.
-
-=head1 EXAMPLES
-
-=head2 Basic usage (pdistance values)
-
-PopGen-db_loadSeqID.pl -p -da PopGen-db.sqlite -r mazei_I_2.0_c_0.4_m_maxbit < pdist_res.txt
-
-=head1 AUTHOR
-
-Nick Youngblut <nyoungb2@illinois.edu>
-
-=head1 AVAILABILITY
-
-sharchaea.life.uiuc.edu:/home/git/ITEP_PopGen/
-
-=head1 COPYRIGHT
-
-Copyright 2010, 2011
-This software is licensed under the terms of the GPLv3
-
-=cut
 
--- blib/script/orthoOutlierDetect.pl
+++ blib/script/orthoOutlierDetect.pl
@@ -20,6 +20,10 @@ orthoOutlierDetect.pl [flags] *fasta > outliers.txt
 
 =over
 
+=item -table  <char>
+
+3-column, tab-delimite table (file\tITEP_runID\tclusterID). 
+
 =item -ceiling  <float>
 
 Outlier must have lower seqID than '-ceiling' [99.9]
@@ -64,9 +68,19 @@ in half. Outliers in both seqID & gaps (similar to seqID outlier detection)
 for 1 half but not the other will be flagged as possible truncations that
 produced the observed sequence divergence in the alignment.
 
+=head3 file table
+
+3 columns: file\tITEP_runID\tclusterID
+
+Needed for getting min distance to contig end.
+
 =head3 output columns
 
 =over
+
+=item * ITEP cluster run ID
+
+=item * ITEP cluster ID
 
 =item * file_name
 
@@ -82,13 +96,29 @@ produced the observed sequence divergence in the alignment.
 
 =item * truncation_half
 
+=item * min-distance from contig end (1st side)
+
+=item * min-distance from contig end (2nd side)
+
 =back
+
+=head2 min-distance
+
+format = 'comparison'__'distance in bp'
+
+'g' = gene, 'c' = contig
+
+'s' = start, 'e' = end
 
 =head1 EXAMPLES
 
 =head2 Basic usage:
 
 orthoOutlierDetect.pl gene_cluster.fasta > outliers.txt
+
+=head2 Using a file table
+
+orthoOutlierDetect.pl -t files.txt > outliers.txt
 
 =head2 Many fasta files
 
@@ -121,11 +151,12 @@ use File::Spec;
 use Bio::AlignIO;
 use Statistics::Descriptive;
 use Parallel::ForkManager;
+use List::Util qw/min/;
 
 #--- args/flags ---#
 pod2usage("$0: No files given.") if ((@ARGV == 0) && (-t STDIN));
 
-my ($verbose_b);
+my ($verbose_b, $file_tbl);
 my $ceiling = 99.9;
 my $floor = 4;
 my $stdev_x = 1;
@@ -135,21 +166,33 @@ GetOptions(
 	"floor=i" => \$floor,
 	"x=f" => \$stdev_x,
 	"processors=i" => \$fork,
+	"table=s" => \$file_tbl,
 	"verbose" => \$verbose_b,
 	"help|?" => \&pod2usage # Help
 	);
 
 #--- I/O error & defaults ---#
-my @files = @ARGV;
-map{ die "ERROR: cannot find file!\n" unless -e $_ } @files;
+my $file_index_r;		# file => [ITEP_runID, clusterID]
+if(defined $file_tbl){ # files provided via table #
+	$file_index_r = load_file_table($file_tbl);
+	}
+else{			# files provided via ARGV
+	my %tmp;
+	map{ $tmp{$_} = [0,0] } @ARGV;
+	$file_index_r = \%tmp;
+	}
+map{ die "ERROR: cannot find file!\n" unless -e $_ } keys %$file_index_r;
 
 
 #--- MAIN ---#
+# getting contigs if file table provided #
+my $contigs_r = get_contigs($file_index_r) if defined $file_tbl;
+
+# running through each gene cluster #
 my $pm = new Parallel::ForkManager($fork);
-foreach my $file (@files){
+foreach my $file (sort keys %$file_index_r ){
 	# forking #
 	my $pid = $pm->start and next;
-
 
 	# status #
 	print STDERR "Processing: $file\n";	
@@ -165,7 +208,7 @@ foreach my $file (@files){
 	    my $means_r = get_mean_seqID_indiv($pIDs_r);
 	    my $outliers_r = flag_outliers(\%output, $file, $means_r, $mean, $stdev, $stdev_x, $ceiling);
 	    
-	    # checking for a trucation #
+	    # if outliers found: checking for a trucation #
 	    if(%$outliers_r){		
 	    	# split alignment #
 	    	my @aln_halves = split_aln($alno);
@@ -189,10 +232,22 @@ foreach my $file (@files){
 	    		my $means_r = get_mean_seqID_indiv($pIDs_r);
 	 			flag_outliers_ret(\%half_stats, $i, $means_r, $mean, $stdev, $stdev_x, $ceiling);
 				}
+			
+			# trucation is flagged #
 			flag_truncation(\%output, $file, \%half_stats);
 	    	}
 	    
-	    write_output(\%output);
+	    # if truncation: getting cluster gene info #
+	    if(%$outliers_r){
+		    my $info_r = get_cluster_gene_info( $file_index_r->{$file} );
+	    
+		    # check location relative to contig end #
+	    	## ITEP-dependent ##
+		    get_min_dist2contig_end($outliers_r, $contigs_r, $info_r, \%output);	    
+		    }
+	    
+	    # writing output table #
+	    write_output(\%output, $file_index_r->{$file});
 	    }
 	
 	$pm->finish;
@@ -202,15 +257,166 @@ foreach my $file (@files){
 
 #--- Subroutines ---#
 sub write_output{
-	my $output_r = shift;
+	my ($output_r, $ITEP_r) = @_;
 	
 	foreach my $id (keys %$output_r){
 		foreach my $index (keys %{$output_r->{$id}}){
-			map{ $_ = 'NA' unless defined $_ } @{$output_r->{$id}{$index}}[0..6];
+			
+			map{ $_ = 'NA' unless defined $_ } @{$output_r->{$id}{$index}}[0..8];
 			next if ${$output_r->{$id}{$index}}[0] eq "NA";
-			print join("\t", @{$output_r->{$id}{$index}}), "\n";
+
+			print join("\t", @$ITEP_r,
+					@{$output_r->{$id}{$index}}), "\n";
 			}
 		}
+	}
+
+sub get_min_dist2contig_end{
+	my ($outliers_r, $contigs_r, $info_r, $output_r) = @_;
+	
+	# status #
+	print STDERR "\tFinding min distance to contig end\n";
+	
+	# foreach outlier (taxon), get scaffold-start-stop from gene info table #
+	my %loc;
+	foreach my $taxon (keys %$outliers_r){
+		die "ERROR: cannot find $taxon in geneInfo table!\n"
+			unless exists $info_r->{$taxon};
+		
+		$loc{$taxon}{'scaf'} = ${$info_r->{$taxon}}[4];
+		$loc{$taxon}{'start'} = ${$info_r->{$taxon}}[5];
+		$loc{$taxon}{'end'} = ${$info_r->{$taxon}}[6];
+		$loc{$taxon}{'strand'} = ${$info_r->{$taxon}}[7];
+		}
+
+		#print Dumper %loc; #exit;
+
+	# foreach outlier (taxon), getting min distance to end of scaffold #
+	my %min_dists;
+	foreach my $taxon (keys %loc){
+		my %dists;
+		my $scaf = $loc{$taxon}{'scaf'};
+		my $strand = $loc{$taxon}{'strand'};
+		if( $strand eq '+' ){		# + strand
+			# gene start vs contig start #
+			foreach my $pos ( @{$contigs_r->{$scaf}{'start'}} ){
+				push @{$dists{'gs-cs'}}, abs($loc{$taxon}{'start'} - $pos);
+				}
+			# gene end vs contig end #
+			foreach my $pos ( @{$contigs_r->{$scaf}{'end'}} ){
+				push @{$dists{'ge-ce'}}, abs($loc{$taxon}{'end'} - $pos);
+				}
+			# min distance #
+			map{ $min_dists{$taxon}{$_} = min(@{$dists{$_}}) } keys %dists;
+			}
+		elsif( $strand eq '-' ){		# - strand
+			# gene start vs contig end #
+			foreach my $pos ( @{$contigs_r->{$scaf}{'end'}} ){
+				push @{$dists{'gs-ce'}}, abs($loc{$taxon}{'start'} - $pos);
+				}
+			# gene end vs contig start #
+			foreach my $pos ( @{$contigs_r->{$scaf}{'start'}} ){
+				push @{$dists{'ge-cs'}}, abs($loc{$taxon}{'end'} - $pos);
+				}
+			# min distance #
+			map{ $min_dists{$taxon}{$_} = min(@{$dists{$_}}) } keys %dists;
+			}
+		else{ die "LOGIC ERROR! $!\n"; }
+		}
+	
+	# adding to output #
+	foreach my $id (keys %$output_r){
+		foreach my $index (keys %{$output_r->{$id}}){
+			foreach my $comp ( keys %{$min_dists{$id}} ){
+				push @{$output_r->{$id}{$index}}, ('NA') x 2
+					unless defined ${$output_r->{$id}{$index}}[5]
+						&& defined ${$output_r->{$id}{$index}}[6];		# 'NA' if 'possible_truction' & '..._half' columns missing
+				push @{$output_r->{$id}{$index}}, join("__", $comp, $min_dists{$id}{$comp});
+				}
+			}
+		}
+	
+		#print Dumper $output_r; #exit;
+	}
+
+sub get_cluster_gene_info{
+	my ( $ITEP_r ) = @_;
+	
+	my $cmd = join("", "echo '", join("\t", @$ITEP_r),
+					"' | db_getClusterGeneInformation.py |");
+					
+	open PIPE, $cmd or die $!;
+	my %info;
+	while(<PIPE>){
+		chomp;
+		next if /^\s*$/;
+		my @l = split /\t/;
+		$l[1] =~ s/[ .]/_/g;
+		$info{$l[1]} = \@l;			# key = taxon_name
+		}
+	close PIPE;
+	
+		#print Dumper %info; exit;
+	return \%info;
+	}
+
+sub load_file_table{
+	my ($file_tbl) = @_;
+	open IN, $file_tbl or die $!;
+	my %file_index;
+	while(<IN>){
+		chomp;
+		next if /^\s*$/;
+		my @l = split /\t/;
+		die "ERROR: line $. of file table does not have 3 columns!\n"
+			unless scalar @l == 3;
+		$file_index{$l[0]} = [@l[1..2]];
+		}
+	close IN;
+	return \%file_index;
+	}
+
+sub get_contigs{
+# calling db_getContigs.py #
+	my ($file_index_r) = @_;
+
+	# status #
+	warn "### Getting contig/scaffold start-stop ###\n";
+
+	# getting run IDs #	
+	my %run_ids;
+	map{ $run_ids{ ${$file_index_r->{$_}}[0] } = 1 } keys %$file_index_r;
+	
+	# getting orgs #
+	my %contig;
+	foreach my $run_id (keys %run_ids){
+		open PIPE, "echo $run_id | db_getOrganismsInClusterRun.py | db_getContigs.py | db_getContigSeqs.py | " or die $!;
+	
+		while(<PIPE>){
+			chomp;
+			next if /^\s*$/;
+			my @l = split /\t/; 
+
+			# getting locations of gaps #
+			my $i = 0;
+			while($l[1] =~ /N+/gi){
+				$i++;
+				#$contig{$l[0]}{"gap$i"}{"start"} = $-[0] + 1;
+				#$contig{$l[0]}{"gap$i"}{"end"} = $+[0] + 1;
+				push @{$contig{$l[0]}{'start'}}, int($-[0] + 1);		# contig_start
+				push @{$contig{$l[0]}{'end'}}, int($+[0] + 1);		# contig_end
+				}
+			# scaffold start-end #
+				#$contig{$l[0]}{"scaf"}{"start"} = 1;
+				#$contig{$l[0]}{"scaf"}{"stop"} = length($l[1]);
+			push @{$contig{$l[0]}{'start'}}, 1;
+			push @{$contig{$l[0]}{'end'}}, length($l[1]);
+			}
+		}
+	close PIPE;
+		
+		#print Dumper %contig; exit;
+	return \%contig;		# start-stop 1-indexed
 	}
 
 sub flag_truncation{
@@ -401,12 +607,12 @@ sub pairwise_percent_identity{
 # getting array of pairwise identities # 
 	my ($alno) = @_;
 	
-	#print Dumper $alno->length; exit;
 	my @seqs = $alno->each_seq();
 	
 	my @pIDs;
 	for my $i (0..$#seqs){
 		for my $ii (0..$#seqs){
+				#print STDERR ".";
 			next if $i >= $ii;
 			my $alno_p = $alno->select($i+1, $ii+1);
 			
@@ -420,4 +626,27 @@ sub pairwise_percent_identity{
 	return \@pIDs;
 	}
 
+sub get_org_list_OLD{
+	my ($file_index_r) = @_;
+	
+	
+	
+	# getting run IDs #	
+	my %run_ids;
+	map{ $run_ids{ ${$file_index_r->{$_}}[0] } = 1 } keys %$file_index_r;
+	
+	# getting orgs #
+	my %org_list;
+	foreach my $run_id (keys %run_ids){
+		open PIPE, "echo $run_id | db_getOrganismsInClusterRun.py |" or die $!;
+		while(<PIPE>){
+			chomp;
+			next if /^\s*$/;
+			push @{$org_list{$run_id}}, $_;
+			}
+		close PIPE;
+		}
 
+		print Dumper %org_list; exit;
+	return \%org_list;
+	}
